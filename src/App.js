c23fee6629afe0405cbc4571e636103ab4d44d51
simonkaruga <simonkaruganjoroge@gmail.com>
2025-10-27 09:15:39 +0300

    Add origin

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// src/components/App.js
+import Header from "./components/Header";
+import About from "./components/About";
+import ArticleList from "./components/ArticleList";
+import blogData from "./data/blog";
+
+function App() {
+  const { name, image, about, posts } = blogData;
+
+  return (
+    <div className="App">
+      <Header name={name} />
+      <About image={image} about={about} />
+      <ArticleList posts={posts} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Article.js
@@ -0,0 +1,26 @@
+// src/components/Article.js
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  // 🧮 Bonus: 'Minutes to Read'
+  let emoji = "";
+  if (minutes < 30) {
+    const cups = Math.ceil(minutes / 5);
+    emoji = "☕️".repeat(cups);
+  } else {
+    const bentos = Math.ceil(minutes / 10);
+    emoji = "🍱".repeat(bentos);
+  }
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>
+        {date} • {emoji} {minutes} min read
+      </small>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
